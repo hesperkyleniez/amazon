c9228612a539d169ea861d25e2117dec7fe0c8ce
kyle-niez <kyleniez30@gmail.com>
2026-09-17 22:17:20 +0800

    commit

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,7 +2,7 @@ const products = [{
   image: images/products/athletic-cotton-socks-6-pairs.jpg,
   name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
   rating: {
-    stars: 4.5,
+    stars: 4.0,
     count: 87
   },
   priceCents: 1090
